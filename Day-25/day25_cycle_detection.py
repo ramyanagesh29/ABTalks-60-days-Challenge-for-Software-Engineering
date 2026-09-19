6b594b3b64c6d41eefda14d1735bcccca3b29fd3
ramyanagesh29 <ramyanagesh05@gmail.com>
2026-09-19 14:29:50 +0530

    Create  day25_cycle_detection.py

---
+++ Day-25/day25_cycle_detection.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        current = self.head
+
+        while current.next is not None:
+            current = current.next
+
+        current.next = new_node
+
+
+def has_cycle(head):
+    # Your Floyd cycle detection logic here
+    pass
